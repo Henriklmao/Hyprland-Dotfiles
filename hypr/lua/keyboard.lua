9805e962283f4fe8b9dbc335663d9367be32c7ac
Henrik Bernhardt <57109108+Henriklmao@users.noreply.github.com>
2026-06-09 11:17:43 +0200

    fix(touchpad): enabled tochpad while typing

--- hypr/lua/keyboard.lua
+++ hypr/lua/keyboard.lua
@@ -1,16 +1,17 @@
 -- Keyboard Layout
 hl.config({
-    input = {
-        kb_layout = "de",
-        kb_options = "compose:caps",
-        follow_mouse = 1,
-        sensitivity = 0,
-        repeat_rate = 40,
-        repeat_delay = 600,
-        numlock_by_default = true,
-        touchpad = {
-            natural_scroll = true,
-            scroll_factor = 0.4
-        }
-    }
+	input = {
+		kb_layout = "de",
+		kb_options = "compose:caps",
+		follow_mouse = 1,
+		sensitivity = 0,
+		repeat_rate = 40,
+		repeat_delay = 600,
+		numlock_by_default = true,
+		touchpad = {
+			natural_scroll = true,
+			scroll_factor = 0.4,
+			disable_while_typing = false,
+		},
+	},
 })
