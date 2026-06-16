34d2684bfbb24ec3172818fb7cb31aa5fe1a6e1c
Henrik Bernhardt <57109108+Henriklmao@users.noreply.github.com>
2026-06-16 11:32:55 +0200

    refactor: removed old .conf files and added auto wipe of cliphist
    I though it was memory leaks. It was just my cliphist being filled to
    the brimm

--- hypr/lua/autostart.lua
+++ hypr/lua/autostart.lua
@@ -1,26 +1,27 @@
 -- Autostart
 hl.on("hyprland.start", function()
-    -- From autostart.conf
-    hl.exec_cmd("~/.config/ml4w/listeners.sh --startall")
-    hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
-    hl.exec_cmd("~/.config/ml4w/scripts/ml4w-wallpaper-app --restore")
-    hl.exec_cmd("~/.config/ml4w/scripts/ml4w-autostart")
-    hl.exec_cmd("~/.config/hypr/scripts/gtk.sh")
-    hl.exec_cmd("swaync")
-    hl.exec_cmd("hypridle")
-    hl.exec_cmd("wl-paste --watch cliphist store")
-    hl.exec_cmd("~/.config/hypr/scripts/cleanup.sh")
-    hl.exec_cmd("~/.config/com.ml4w.hyprlandsettings/hyprctl.sh")
+	-- From autostart.conf
+	hl.exec_cmd("~/.config/ml4w/listeners.sh --startall")
+	hl.exec_cmd("/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1")
+	hl.exec_cmd("~/.config/ml4w/scripts/ml4w-wallpaper-app --restore")
+	hl.exec_cmd("~/.config/ml4w/scripts/ml4w-autostart")
+	hl.exec_cmd("~/.config/hypr/scripts/gtk.sh")
+	hl.exec_cmd("swaync")
+	hl.exec_cmd("hypridle")
+	hl.exec_cmd("wl-paste --watch cliphist store")
+	hl.exec_cmd("~/.config/hypr/scripts/cleanup.sh")
+	hl.exec_cmd("~/.config/com.ml4w.hyprlandsettings/hyprctl.sh")
 
-    -- Ensure QuickShell runs with the user's profile so Super+Esc targets the correct instance
-    hl.exec_cmd("qs -p ~/.config/quickshell")
-    
-    -- From HenrikHyprkey_Standard.conf
-    hl.exec_cmd("/usr/lib/polkit-kde-authentication-agent-1")
-    hl.exec_cmd("kdeconnect-indicator")
-    hl.exec_cmd("/usr/bin/kdeconnectd")
-    
-    -- GTK Settings
-    hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
-    hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'")
+	-- Ensure QuickShell runs with the user's profile so Super+Esc targets the correct instance
+	hl.exec_cmd("qs -p ~/.config/quickshell")
+
+	-- From HenrikHyprkey_Standard.conf
+	hl.exec_cmd("/usr/lib/polkit-kde-authentication-agent-1")
+	hl.exec_cmd("kdeconnect-indicator")
+	hl.exec_cmd("/usr/bin/kdeconnectd")
+
+	-- GTK Settings
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'")
+	hl.exec_cmd("gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3'")
+	hl.exec_cmd("cliphist wipe")
 end)
