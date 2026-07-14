c2b20e9471c9859c3c0ae2963330d913e70cad5b
Henrik Bernhardt <57109108+Henriklmao@users.noreply.github.com>
2026-07-14 13:06:30 +0200

    feat: add tmux compatability and outline

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,11 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- for TMUX:
+vim.api.nvim_create_autocmd("DirChanged", {
+  callback = function()
+    local cwd = vim.fn.getcwd()
+    vim.fn.chdir(cwd)
+  end,
+})
