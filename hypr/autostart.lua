f82a42dcd716cb8a8badae67bb60721b8aaebf0f
Henrik Bernhardt <57109108+Henriklmao@users.noreply.github.com>
2026-08-27 12:50:32 +0200

    hypr: add dotfiles auto-update on boot

--- hypr/autostart.lua
+++ hypr/autostart.lua
@@ -2,6 +2,18 @@
 -- o.launch_on_start("my-service")
 o.launch_on_start("kdeconnectd")
 o.launch_on_start("kdeconnect-indicator")
+
+hl.exec_cmd([[bash -c '
+  output=$(cd ~/Dotfiles && git pull 2>&1)
+  if [ $? -ne 0 ]; then
+    notify-send "Dotfiles Update" "Manual intervention required" -u critical
+    kitty --class lazygit sh -c 'cd ~/Dotfiles && lazygit'
+  elif echo "$output" | grep -q "Already up to date"; then
+    true
+  else
+    notify-send "Dotfiles Update" "Updated successfully" -u low
+  fi
+']])
 if not IS_SURFACE then
 	hl.exec_cmd("headsetcontrol -s 115")
 end
